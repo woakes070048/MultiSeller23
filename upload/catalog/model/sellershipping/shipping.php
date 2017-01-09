0dc1d40818a7a3ef7ea4286c16a51edd8b0b5aa9
alkod <insh1982@gmail.com>
2017-01-09 21:54:23 +0300

    Add more seller shipping methods

--- upload/catalog/model/sellershipping/shipping.php
+++ upload/catalog/model/sellershipping/shipping.php
@@ -112,6 +112,94 @@ class ModelSellerShippingShipping extends Model {
 
 		}
 
+		if($shipping == 'free'){
+
+			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('free_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+
+			if (!$this->config->get($seller_id.':free_geo_zone_id')) {
+				$status = true;
+			} elseif ($query->num_rows) {
+				$status = true;
+			} else {
+				$status = false;
+			}
+
+			if ($this->cart->getSubTotal() < $this->config->get($seller_id.':free_total')) {
+				$status = false;
+			}
+
+			$method_data = array();
+
+			if ($status) {
+				$quote_data = array();
+
+				$quote_data['free'] = array(
+					'code'         => $seller_id.':free.free',
+					'title'        => $this->language->get('text_description'),
+					'cost'         => 0.00,
+					'tax_class_id' => 0,
+					'text'         => $this->currency->format(0.00, $this->session->data['currency'])
+				);
+
+				$method_data = array(
+					'code'       => 'free',
+					'title'      => $this->language->get($seller_id.':text_title'),
+					'quote'      => $quote_data,
+					'sort_order' => $this->config->get($seller_id.':free_sort_order'),
+					'error'      => false
+				);
+			}
+
+			return $method_data;
+
+		}
+
+		if($shipping == 'item'){
+
+			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('item_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+
+			if (!$this->config->get($seller_id.':item_geo_zone_id')) {
+				$status = true;
+			} elseif ($query->num_rows) {
+				$status = true;
+			} else {
+				$status = false;
+			}
+
+			$method_data = array();
+
+			if ($status) {
+				$items = 0;
+
+				foreach ($this->model_sellerproduct_product->getProductsOnCartBySellerId($seller_id) as $product) {
+					if ($product['shipping']) {
+						$items += $product['quantity'];
+					}
+				}
+
+				$quote_data = array();
+
+				$quote_data['item'] = array(
+					'code'         => $seller_id.':item.item',
+					'title'        => $this->language->get('text_description'),
+					'cost'         => $this->config->get($seller_id.':item_cost') * $items,
+					'tax_class_id' => $this->config->get($seller_id.':item_tax_class_id'),
+					'text'         => $this->currency->format($this->tax->calculate($this->config->get($seller_id.':item_cost') * $items, $this->config->get($seller_id.':item_tax_class_id'), $this->config->get('config_tax')), $this->session->data['currency'])
+				);
+
+				$method_data = array(
+					'code'       => 'item',
+					'title'      => $this->language->get($seller_id.':text_title'),
+					'quote'      => $quote_data,
+					'sort_order' => $this->config->get($seller_id.':item_sort_order'),
+					'error'      => false
+				);
+			}
+
+			return $method_data;
+
+		}
+
 
 	}
 
